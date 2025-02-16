2365f1be1c722a63c9cff8892f1f0312eadaa35f
Rudrank Riyam <rudrankriyam@gmail.com>
2025-02-16 15:50:09 +0530

    Add platform support and MLX dependencies to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,18 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "NazorKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NazorKit",
-            targets: ["NazorKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NazorKit"),
-
-    ]
+  name: "NazorKit",
+  platforms: [
+    .iOS(.v16),
+    .macOS(.v13),
+    .visionOS(.v1),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "NazorKit",
+      targets: ["NazorKit"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/ml-explore/mlx-swift-examples.git", branch: "main")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "NazorKit",
+      dependencies: [
+        .product(name: "MLXVLM", package: "mlx-swift-examples"),
+        .product(name: "MLXLMCommon", package: "mlx-swift-examples"),
+      ])
+  ]
 )
