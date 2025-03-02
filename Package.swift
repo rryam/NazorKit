32774ae192968c4cb2b395a21d98c24ac2e1890d
Rudrank Riyam <rudrankriyam@gmail.com>
2025-03-02 23:06:00 +0530

    Solve Swift 6 issues

--- Package.swift
+++ Package.swift
@@ -7,11 +7,10 @@ let package = Package(
   name: "NazorKit",
   platforms: [
     .iOS(.v16),
-    .macOS(.v13),
+    .macOS(.v14),
     .visionOS(.v1),
   ],
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "NazorKit",
       targets: ["NazorKit"])
@@ -20,8 +19,6 @@ let package = Package(
     .package(url: "https://github.com/ml-explore/mlx-swift-examples.git", branch: "main")
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "NazorKit",
       dependencies: [
